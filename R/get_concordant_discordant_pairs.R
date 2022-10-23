7e8c448b2bbbeb04a968f333311bb95bcc3ff0d4
Meshach <58997358+joe-chelladurai@users.noreply.github.com>
2022-10-22 21:17:12 -0600

    Update

--- R/get_concordant_discordant_pairs.R
+++ R/get_concordant_discordant_pairs.R
@@ -4,8 +4,8 @@
 #'
 #' @param .data = data
 #' @param id = id column
-#' @param var1 variable 1
-#' @param var2 variable 2
+#' @param var_1 variable 1
+#' @param var_2 variable 2
 #' @return a data frame
 #' @importFrom tidyr pivot_longer pivot_wider
 #' @importFrom dplyr mutate case_when count select
@@ -15,29 +15,29 @@
 #' @export
 #' @examples
 #' mtcars$id <- seq.int(nrow(mtcars))
-#' get_concordant_discordant_pairs(mtcars, id = id, var1 = vs, var2 = am)
+#' get_concordant_discordant_pairs(mtcars, id = id, var_1 = vs, var_2 = am)
 
 
 
-get_concordant_discordant_pairs <- function(.data, id, var1, var2) {
+get_concordant_discordant_pairs <- function(.data, id, var_1, var_2) {
 
-  var1_name <- deparse(substitute(var1))
-  var2_name <- deparse(substitute(var2))
+  var_1_name <- deparse(substitute(var_1))
+  var_2_name <- deparse(substitute(var_2))
 
   table <-   .data %>% pivot_longer(cols = -{{id}}) %>%
     mutate(value = as.numeric(factor(value))-1) %>%
     pivot_wider() %>%
     mutate(pairs = case_when(
-      {{var1}} > {{var2}} ~ "b",
-      {{var1}} < {{var2}} ~ "c",
-      {{var1}} + {{var2}} == 2 ~ "a",
-      {{var1}} + {{var2}} == 0 ~ "d")) %>%
-    mutate(type = case_when(pairs == "a" ~ "concordant: var1 = 1 & var2 = 1",
-                            pairs == "d" ~ "concordant: var1 = 0 & var2 = 0",
-                            pairs == "b" ~ "discordant: var1 = 1 & var2 = 0",
-                            pairs == "c" ~ "discordant: var1 = 0 & var2 = 1")) %>%
-    mutate(type = str_replace(type, 'var1', var1_name)) %>%
-    mutate(type = str_replace(type, 'var2', var2_name)) %>%
+      {{var_1}} > {{var_2}} ~ "b",
+      {{var_1}} < {{var_2}} ~ "c",
+      {{var_1}} + {{var_2}} == 2 ~ "a",
+      {{var_1}} + {{var_2}} == 0 ~ "d")) %>%
+    mutate(type = case_when(pairs == "a" ~ "concordant: var_1 = 1 & var_2 = 1",
+                            pairs == "d" ~ "concordant: var_1 = 0 & var_2 = 0",
+                            pairs == "b" ~ "discordant: var_1 = 1 & var_2 = 0",
+                            pairs == "c" ~ "discordant: var_1 = 0 & var_2 = 1")) %>%
+    mutate(type = str_replace(type, 'var_1', var_1_name)) %>%
+    mutate(type = str_replace(type, 'var_2', var_2_name)) %>%
     count(pairs, type) %>% data.frame()
 
 
