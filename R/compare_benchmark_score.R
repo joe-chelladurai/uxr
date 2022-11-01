39a31213e6ffbcb8bca8898ae2c4fdd9e8558917
Meshach <58997358+joe-chelladurai@users.noreply.github.com>
2022-10-31 23:33:57 -0600

    Update

--- R/compare_benchmark_score.R
+++ R/compare_benchmark_score.R
@@ -46,24 +46,54 @@ compare_benchmark_score <- function(data, benchmark, alpha, tail = "one", remove
     lower_ci <- mean - margin_of_error
     upper_ci <- mean + margin_of_error
 
-    data.frame(mean = mean,
+    text_output <-  paste("We can be",
+                        paste0(round(confidence, 2)*100, "%"),
+                        "confident that the true score is between",
+                        round(lower_ci, 2), "and",
+                        round(upper_ci, 2))
+
+    result <- data.frame(mean = mean,
          sd = sd,
          se = se,
          n = n,
          df = df,
          alpha = alpha,
-         probability = probability,
+         probability = round(probability, 3),
          tail = tail,
-         confidence = confidence,
+         confidence = round(confidence, 3),
          margin_of_error = margin_of_error,
-         t = t,
+         t = round(t, 3),
          lower_ci = lower_ci,
          upper_ci = upper_ci,
-         text_output = paste("We can be",
-                             paste0(round(confidence, 2)*100, "%"),
-                                   "confident that the true score is between",
-                                   round(lower_ci, 2), "and",
-                                   round(upper_ci, 2)))
+         text_output = text_output)
+
+
+    cli::cli_h1("Compare Score with a Benchmark")
+
+
+
+
+
+    cli::cli_text(result$text_output)
+
+    result2 <- result |>
+      t() |>
+      data.frame() |>
+      tibble::rownames_to_column("term") |>
+      data.frame() |>
+      dplyr::rename(result = t.result.) |>
+      huxtable::as_hux()
+
+    huxtable::position(result2) <- "left"
+
+    result3 <- result2 |> dplyr::filter(!stringr::str_detect(term, "text_output"))
+    result3 <- huxtable::map_align(result3, huxtable::by_cols("left", "right"))
+
+    huxtable::print_screen(result3, colnames = FALSE)
+
+    result4 <- data.frame(result2)
+
+    return(invisible(result4))
   }
 
 
