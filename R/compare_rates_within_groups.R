d1cfdf2643d53b1c36e2666afde2768352a56c93
Meshach <58997358+joe-chelladurai@users.noreply.github.com>
2022-12-23 02:50:02 -0700

    Update

--- R/compare_rates_within_groups.R
+++ R/compare_rates_within_groups.R
@@ -6,6 +6,8 @@
 #' @param x var 1
 #' @param y var 2
 #' @param conf_level Confidence level
+#' @param input input type currently only accepts "wide"
+#' @param output Default is "console", also accepts "tibble"
 #' @importFrom dplyr bind_cols rename
 #' @importFrom tibble as_tibble
 #' @return results
@@ -14,9 +16,11 @@
 #' A <- c(1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1)
 #' B <- c(0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0)
 #' data <- data.frame(A, B)
+#' compare_rates_within_groups(data, A, B, input = "wide")
 
+compare_rates_within_groups <- function(data, x, y, conf_level = 0.95, input, output = "console") {
 
-compare_rates_within_groups <- function(data, x, y, conf_level = 0.95) {
+  if(missing(input)) { stop("please specify input = 'wide'")}
 
   conf_int <- get_confidence_intervals_within_groups(data, x, y, conf_level)
 
@@ -24,6 +28,22 @@ compare_rates_within_groups <- function(data, x, y, conf_level = 0.95) {
 
   result <- bind_cols(test, conf_int)
 
-  return(result)
+
+  if(output == "console") {
+    cli::cli_h1("Compare Rates Within Groups")
+    cli::cli_alert("McNemar's Test")
+    result_print <- result |> as_hux()
+    huxtable::position(result_print) <- "left"
+    result_print <- map_align(result_print, by_cols("left", "right"))
+    print_screen(result_print, colnames = FALSE)
+    return(invisible(result))
+  } else if (output == "tibble") {
+    return(result)
+  }
 
 }
+
+
+
+
+
