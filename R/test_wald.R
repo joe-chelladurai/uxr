06244b0561e435ec6ec139542741146b2022b8ae
Meshach <58997358+joe-chelladurai@users.noreply.github.com>
2022-12-23 12:29:04 -0700

    Update

--- R/test_wald.R
+++ R/test_wald.R
@@ -3,19 +3,21 @@
 #'
 #' @param success success
 #' @param total total
-#' @param ci_z z value
+#' @param conf_level conf_level (default: 0.95)
 #'
 #' @return lower_ci, upper_ci
 #' @export
 #' @examples
-#' test_wald(10, 12, 1.96)
-#' test_wald(5, 7, 1.96)
+#' test_wald(10, 12, 0.95)
+#' test_wald(5, 7, 0.95)
 
-test_wald <- function(success, total, ci_z) {
+test_wald <- function(success, total, conf_level = 0.95) {
+
+  z <- abs(qnorm((1 - conf_level) / 2))
 
   p <- success/total
 
-  value <- ci_z*(sqrt((p*(1-p)/total)))
+  value <- z*(sqrt((p*(1-p)/total)))
 
   lower_ci <- p - value
   upper_ci <- p + value
--- R/test_wald_adj.R
+++ R/test_wald_adj.R
@@ -5,31 +5,36 @@
 #'
 #' @param success success
 #' @param total total
-#' @param ci_z z value
-#'
+#' @param conf_level conf_level (default: 0.95)
 #' @return lower_ci, upper_ci
 #' @export
 #' @examples
-#' test_wald_adj(10, 12, 1.96)
-#' test_wald_adj(5, 7, 1.96)
+#' test_wald_adj(10, 12, 0.95)
+#' test_wald_adj(5, 7, 0.95)
 
 
 
-test_wald_adj <- function(success, total, ci_z) {
+test_wald_adj <- function(success, total, conf_level = 0.95) {
 
 
-  adjusted_proportion <- function(success, total, ci_z) {
-    p_adj <- (success + ci_z^2/2)/(total + ci_z^2)
+  z <- abs(qnorm((1 - conf_level) / 2))
+
+  adjusted_proportion <- function(success, total, conf_level) {
+
+    z <- abs(qnorm((1 - conf_level) / 2))
+
+    p_adj <- (success + z^2/2)/(total + z^2)
     p_adj
   }
 
-  p_adj <- adjusted_proportion(success, total, ci_z)
+  p_adj <- adjusted_proportion(success, total, conf_level)
 
 
   p <- p_adj
-  total <- total + ci_z*2
 
-  value <- ci_z*(sqrt((p*(1-p)/total)))
+  total <- total + z*2
+
+  value <- z*(sqrt((p*(1-p)/total)))
 
   lower_ci <- p - value
   upper_ci <- p + value
